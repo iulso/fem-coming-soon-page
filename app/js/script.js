fdb12d5f0548188b36168f58cce0b923a1176aad
Julia S <20336015+juliasj@users.noreply.github.com>
2023-05-12 18:46:09 +0200

    Start

---
+++ app/js/script.js
@@ -0,0 +1,9 @@
+console.log('hola');
+
+const btn = document.getElementById('subscribe');
+
+btn.addEventListener('click', function (e) {
+  e.preventDefault();
+});
+
+// https://www.youtube.com/watch?v=z351aUaYZm0
---
+++ dist/script.js
@@ -0,0 +1,2 @@
+"use strict";console.log("hola");var btn=document.getElementById("subscribe");btn.addEventListener("click",(function(e){e.preventDefault()}));
+//# sourceMappingURL=script.js.map
