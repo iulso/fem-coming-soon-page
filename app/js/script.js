fb1f05990328527e2779e8da6ac577da6f5950d5
Julia S <20336015+juliasj@users.noreply.github.com>
2023-05-15 16:18:49 +0200

    Message styles & JavaScript

--- app/js/script.js
+++ app/js/script.js
@@ -4,7 +4,20 @@ const emailInput = document.getElementById('email');
 const btn = document.getElementById('subscribe');
 const msg = document.querySelector('.coming__msg');
 
-// TODO: check if input empty & msg
+const messages = {
+  error: {
+    class: 'coming__msg--error',
+    text: `Please, provide a valid email address`,
+  },
+  success: {
+    class: 'coming__msg--success',
+    text: `Thank you! We'll be in your inbox soon.`,
+  },
+  info: {
+    class: 'coming__msg--info',
+    text: `You are already subscribed!\nThank you for your interest. We're working on some exciting updates and can't wait to share them with you - stay tuned!`,
+  },
+};
 
 const getEmailVal = () => {
   const val = emailInput.value;
@@ -21,10 +34,8 @@ const validate = () => {
   const emailValue = getEmailVal();
 
   if (validateEmail(emailValue)) {
-    console.log('Email is valid, continue with form submission');
     return true;
   } else {
-    alert('Email is invalid, skip form submission');
     return false;
   }
 };
@@ -33,16 +44,21 @@ const isEmail = (emails, email) => {
   return emails.includes(email);
 };
 
-const htmlTemplate = () => {
-  const html = `<span class='coming__msg'>Please, provide a valid email</span>`;
-  return html;
+const cleanMsgClasses = () => {
+  msg.classList.remove(
+    messages.error.class,
+    messages.success.class,
+    messages.info.class
+  );
 };
 
 btn.addEventListener('click', async e => {
   e.preventDefault();
 
   if (validate() === false) {
-    msg.textContent = 'Please, provide a valid email';
+    msg.textContent = messages.error.text;
+    cleanMsgClasses();
+    msg.classList.add(messages.error.class);
     return;
   }
 
@@ -53,16 +69,17 @@ btn.addEventListener('click', async e => {
     const response = await fetch(url);
     const jsonData = await response.json();
     const emails = jsonData.emails;
-    console.log(`Fetching correct: ${emails}`);
 
     const emailFound = isEmail(emails, emailValue);
 
     if (emailFound) {
-      alert(
-        `You are already subscribed!\nThank you for your interest. We're working on some exciting updates and can't wait to share them with you - stay tuned!`
-      );
+      msg.textContent = messages.info.text;
+      cleanMsgClasses();
+      msg.classList.add(messages.info.class);
     } else {
-      console.log('Nuevo email');
+      msg.textContent = messages.success.text;
+      cleanMsgClasses();
+      msg.classList.add(messages.success.class);
     }
   } catch (error) {
     console.error(`Fetching error: ${error.message}`);
